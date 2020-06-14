aa4ed4c316ee34a12cb826e6e9ad8710fa2ce175
kishan0725 <kishansmart0@gmail.com>
2020-06-15 03:40:42 +0530

    Changes api to IMDB

--- main.py
+++ main.py
@@ -7,18 +7,18 @@ import json
 import bs4 as bs
 import urllib.request
 import pickle
+import requests
 
 from tmdbv3api import TMDb
 tmdb = TMDb()
 tmdb.api_key = '5492165c61b1a21c06eb3a3b578a6339'
 
+from tmdbv3api import Movie
+
 # load the nlp model and tfidf vectorizer from disk
 filename = 'nlp_model.pkl'
 clf = pickle.load(open(filename, 'rb'))
 vectorizer = pickle.load(open('tranform.pkl','rb'))
-
-from tmdbv3api import Movie
-tmdb_movie = Movie()
 
 def create_sim():
     data = pd.read_csv('final_data.csv')
@@ -86,22 +86,29 @@ def recommend():
 
 @app.route("/movie/<movie_title>")
 def movie(movie_title):
+    tmdb_movie = Movie()
+    result = tmdb_movie.search(movie_title)
+    movie_id = result[0].id
+    movie_name = result[0].title
     reviews_list = []
     reviews_status = []
-    lower = movie_title.lower()
-    movie_name = lower.replace(" ","_").replace(":","").replace("-","").replace("'","")
-    sauce = urllib.request.urlopen('https://www.rottentomatoes.com/m/{}/reviews'.format(movie_name)).read()
+    response = requests.get('https://api.themoviedb.org/3/movie/{}?api_key={}'.format(movie_id,tmdb.api_key))
+    data_json = response.json()
+    imdb_id = data_json['imdb_id']
+    # lower = movie_title.lower()
+    # movie_name = lower.replace(" ","_").replace(":","").replace("-","").replace("'","")
+    sauce = urllib.request.urlopen('https://www.imdb.com/title/{}/reviews?ref_=tt_ov_rt'.format(imdb_id)).read()
     soup = bs.BeautifulSoup(sauce,'lxml')
-    soup_result = soup.find_all("div",{"class":"the_review"})
+    soup_result = soup.find_all("div",{"class":"text show-more__control"})
+    # soup_result = soup.select('div.text.show-more_control')
     for reviews in soup_result:
-        if reviews.string.replace("\n",""):
-            reviews_list.append(reviews.string.replace("\n",""))
-            movie_review_list = np.array([reviews.string.replace("\n","")])
+        if reviews.string:
+            reviews_list.append(reviews.string)
+            movie_review_list = np.array([reviews.string])
             movie_vector = vectorizer.transform(movie_review_list)
             pred = clf.predict(movie_vector)
             reviews_status.append('Good' if pred else 'Bad')
     movie_reviews = {reviews_list[i]: reviews_status[i] for i in range(len(reviews_list))} 
-    result = tmdb_movie.search(movie_title)
     return render_template('movie.html',scrape_movie=movie_name,result=result[0],reviews=movie_reviews)
 
 if __name__ == '__main__':
