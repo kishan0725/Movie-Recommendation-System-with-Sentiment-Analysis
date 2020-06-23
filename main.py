7480bcc8720e20aa42a1acb3d06532f4bb8477a2
Kishan Lal <kishansmart0@gmail.com>
2020-06-23 13:11:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ clf = pickle.load(open(filename, 'rb'))
 vectorizer = pickle.load(open('tranform.pkl','rb'))
 
 def create_sim():
-    data = pd.read_csv('final_data.csv')
+    data = pd.read_csv('main_data.csv')
     # creating a count matrix
     cv = CountVectorizer()
     count_matrix = cv.fit_transform(data['comb'])
@@ -66,50 +66,78 @@ def rcmd(m):
 app = Flask(__name__)
 
 @app.route("/")
-def login():
-    return render_template('login.html')
-
-@app.route("/home")
 def home():
     return render_template('home.html')
 
 
 @app.route("/recommend")
 def recommend():
-    movie = request.args.get('movie')
+    movie = request.args.get('movie') # get movie name from the URL
     r = rcmd(movie)
     movie = movie.upper()
-    if type(r)==type('string'):
+    if type(r)==type('string'): # no such movie found in the database
         return render_template('recommend.html',movie=movie,r=r,t='s')
     else:
-        return render_template('recommend.html',movie=movie,r=r,t='l')
+        tmdb_movie = Movie()
+        result = tmdb_movie.search(movie)
+        movie_id = result[0].id
+        movie_name = result[0].title
+        reviews_list = []
+        reviews_status = []
+        response = requests.get('https://api.themoviedb.org/3/movie/{}?api_key={}'.format(movie_id,tmdb.api_key))
+        data_json = response.json()
+        imdb_id = data_json['imdb_id']
+        poster = data_json['poster_path']
+        img_path = 'https://image.tmdb.org/t/p/original{}'.format(poster)
+        if data_json['genres']:
+            genres = []
+            genre_str = ", " 
+            for i in range(0,len(data_json['genres'])):
+                genres.append(data_json['genres'][i]['name'])
+            genre = genre_str.join(genres)
+        sauce = urllib.request.urlopen('https://www.imdb.com/title/{}/reviews?ref_=tt_ov_rt'.format(imdb_id)).read()
+        soup = bs.BeautifulSoup(sauce,'lxml')
+        soup_result = soup.find_all("div",{"class":"text show-more__control"})
+        for reviews in soup_result:
+            if reviews.string:
+                reviews_list.append(reviews.string)
+                movie_review_list = np.array([reviews.string])
+                movie_vector = vectorizer.transform(movie_review_list)
+                pred = clf.predict(movie_vector)
+                reviews_status.append('Good' if pred else 'Bad')
+        movie_reviews = {reviews_list[i]: reviews_status[i] for i in range(len(reviews_list))} 
+        vote_count = "{:,}".format(result[0].vote_count)
+        # convert 10-06-2019 to June 10 2019
+        MONTHS = ['January', 'February', 'Match', 'April', 'May', 'June',
+          'July', 'August', 'September', 'October', 'November', 'December']
+        def date_convert(s):
+            y = s[:4]
+            m = int(s[5:-3])
+            d = s[8:]
+            month_name = MONTHS[m-1]
 
-@app.route("/movie/<movie_title>")
-def movie(movie_title):
-    tmdb_movie = Movie()
-    result = tmdb_movie.search(movie_title)
-    movie_id = result[0].id
-    movie_name = result[0].title
-    reviews_list = []
-    reviews_status = []
-    response = requests.get('https://api.themoviedb.org/3/movie/{}?api_key={}'.format(movie_id,tmdb.api_key))
-    data_json = response.json()
-    imdb_id = data_json['imdb_id']
-    # lower = movie_title.lower()
-    # movie_name = lower.replace(" ","_").replace(":","").replace("-","").replace("'","")
-    sauce = urllib.request.urlopen('https://www.imdb.com/title/{}/reviews?ref_=tt_ov_rt'.format(imdb_id)).read()
-    soup = bs.BeautifulSoup(sauce,'lxml')
-    soup_result = soup.find_all("div",{"class":"text show-more__control"})
-    # soup_result = soup.select('div.text.show-more_control')
-    for reviews in soup_result:
-        if reviews.string:
-            reviews_list.append(reviews.string)
-            movie_review_list = np.array([reviews.string])
-            movie_vector = vectorizer.transform(movie_review_list)
-            pred = clf.predict(movie_vector)
-            reviews_status.append('Good' if pred else 'Bad')
-    movie_reviews = {reviews_list[i]: reviews_status[i] for i in range(len(reviews_list))} 
-    return render_template('movie.html',scrape_movie=movie_name,result=result[0],reviews=movie_reviews)
+            result= month_name + ' ' + d + ' '  + y
+            return result
+        rd = date_convert(result[0].release_date)
+        status = data_json['status']
+        # convert minutes to hours minutes (eg. 148 minutes to 2 hours 28 mins)
+        if data_json['runtime']%60==0:
+            runtime = "{:.0f} hours".format(data_json['runtime']/60)
+        else:
+            runtime = "{:.0f} hours {} minutes".format(data_json['runtime']/60,data_json['runtime']%60)
+        poster = []
+        movie_title_list = []
+        # getting the posters for the recommended movies
+        for movie_title in r:
+            list_result = tmdb_movie.search(movie_title)
+            movie_id = list_result[0].id
+            response = requests.get('https://api.themoviedb.org/3/movie/{}?api_key={}'.format(movie_id,tmdb.api_key))
+            data_json = response.json()
+            poster.append('https://image.tmdb.org/t/p/original{}'.format(data_json['poster_path']))
+        movie_cards = {poster[i]: r[i] for i in range(len(r))}
+        return render_template('recommend.html',movie=movie,mtitle=r,t='l',cards=movie_cards,
+            result=result[0],reviews=movie_reviews,img_path=img_path,genres=genre,vote_count=vote_count,
+            release_date=rd,status=status,runtime=runtime)
 
 if __name__ == '__main__':
     app.run(debug=True)
