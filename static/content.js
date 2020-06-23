2671a65dda3f17d08762cf2dc7cec3be3cdcfbfd
Kishan Lal <kishansmart0@gmail.com>
2020-06-23 13:14:27 +0530

    Update content.js

--- static/content.js
+++ static/content.js
@@ -1,8 +1,4 @@
 function content(e) {
   localStorage.setItem('title', e.getAttribute('title'));
-  window.document.location="movie/"+e.getAttribute('title');
+  window.document.location="recommend?movie="+e.getAttribute('title');
 }
-
-// function setcontent(){
-//     document.getElementById("name").innerHTML = localStorage.getItem('title');
-// }
