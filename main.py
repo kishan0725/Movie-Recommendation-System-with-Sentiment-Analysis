c4d19d389c280c0df4658886c141f9badeadebfa
Kishan Lal <kishansmart0@gmail.com>
2020-06-25 14:38:13 +0530

    Clean code

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import requests
 
 from tmdbv3api import TMDb
 tmdb = TMDb()
-tmdb.api_key = 'YOUR_API_KEY'
+tmdb.api_key = '5492165c61b1a21c06eb3a3b578a6339'
 
 from tmdbv3api import Movie
 
@@ -50,6 +50,32 @@ def rcmd(m):
             l.append(data['movie_title'][a])
         return l
 
+def ListOfGenres(genre_json):
+    if genre_json:
+        genres = []
+        genre_str = ", " 
+        for i in range(0,len(genre_json)):
+            genres.append(genre_json[i]['name'])
+        return genre_str.join(genres)
+
+def date_convert(s):
+    MONTHS = ['January', 'February', 'Match', 'April', 'May', 'June',
+    'July', 'August', 'September', 'October', 'November', 'December']
+    y = s[:4]
+    m = int(s[5:-3])
+    d = s[8:]
+    month_name = MONTHS[m-1]
+
+    result= month_name + ' ' + d + ' '  + y
+    return result
+
+def MinsToHours(duration):
+    if duration%60==0:
+        return "{:.0f} hours".format(duration/60)
+    else:
+        return "{:.0f} hours {} minutes".format(duration/60,duration%60)
+
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -67,54 +93,55 @@ def recommend():
     else:
         tmdb_movie = Movie()
         result = tmdb_movie.search(movie)
+
+        # get movie id and movie title
         movie_id = result[0].id
         movie_name = result[0].title
-        reviews_list = []
-        reviews_status = []
+        
+        # making API call
         response = requests.get('https://api.themoviedb.org/3/movie/{}?api_key={}'.format(movie_id,tmdb.api_key))
         data_json = response.json()
         imdb_id = data_json['imdb_id']
         poster = data_json['poster_path']
         img_path = 'https://image.tmdb.org/t/p/original{}'.format(poster)
-        if data_json['genres']:
-            genres = []
-            genre_str = ", " 
-            for i in range(0,len(data_json['genres'])):
-                genres.append(data_json['genres'][i]['name'])
-            genre = genre_str.join(genres)
+
+        # getting list of genres form json
+        genre = ListOfGenres(data_json['genres'])
+
+        # web scraping to get user reviews from IMDB site
         sauce = urllib.request.urlopen('https://www.imdb.com/title/{}/reviews?ref_=tt_ov_rt'.format(imdb_id)).read()
         soup = bs.BeautifulSoup(sauce,'lxml')
         soup_result = soup.find_all("div",{"class":"text show-more__control"})
+
+        reviews_list = [] # list of reviews
+        reviews_status = [] # list of comments (good or bad)
         for reviews in soup_result:
             if reviews.string:
                 reviews_list.append(reviews.string)
+                # passing the review to our model
                 movie_review_list = np.array([reviews.string])
                 movie_vector = vectorizer.transform(movie_review_list)
                 pred = clf.predict(movie_vector)
                 reviews_status.append('Good' if pred else 'Bad')
-        movie_reviews = {reviews_list[i]: reviews_status[i] for i in range(len(reviews_list))} 
-        vote_count = "{:,}".format(result[0].vote_count)
-        # convert 10-06-2019 to June 10 2019
-        MONTHS = ['January', 'February', 'Match', 'April', 'May', 'June',
-          'July', 'August', 'September', 'October', 'November', 'December']
-        def date_convert(s):
-            y = s[:4]
-            m = int(s[5:-3])
-            d = s[8:]
-            month_name = MONTHS[m-1]
 
-            result= month_name + ' ' + d + ' '  + y
-            return result
+        # combining reviews and comments into dictionary
+        movie_reviews = {reviews_list[i]: reviews_status[i] for i in range(len(reviews_list))} 
+
+        # getting votes with comma as thousands separators
+        vote_count = "{:,}".format(result[0].vote_count)
+        
+        # convert date to readable format (eg. 10-06-2019 to June 10 2019)
         rd = date_convert(result[0].release_date)
+
+        # getting the status of the movie (released or not)
         status = data_json['status']
+
         # convert minutes to hours minutes (eg. 148 minutes to 2 hours 28 mins)
-        if data_json['runtime']%60==0:
-            runtime = "{:.0f} hours".format(data_json['runtime']/60)
-        else:
-            runtime = "{:.0f} hours {} minutes".format(data_json['runtime']/60,data_json['runtime']%60)
+        runtime = MinsToHours(data_json['runtime'])
+
+        # getting the posters for the recommended movies
         poster = []
         movie_title_list = []
-        # getting the posters for the recommended movies
         for movie_title in r:
             list_result = tmdb_movie.search(movie_title)
             movie_id = list_result[0].id
@@ -122,6 +149,8 @@ def recommend():
             data_json = response.json()
             poster.append('https://image.tmdb.org/t/p/original{}'.format(data_json['poster_path']))
         movie_cards = {poster[i]: r[i] for i in range(len(r))}
+
+
         return render_template('recommend.html',movie=movie,mtitle=r,t='l',cards=movie_cards,
             result=result[0],reviews=movie_reviews,img_path=img_path,genres=genre,vote_count=vote_count,
             release_date=rd,status=status,runtime=runtime)
